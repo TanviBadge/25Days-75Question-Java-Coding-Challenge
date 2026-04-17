59aa62ef70621929fbefdeae43587612763cf516
Tanvi Vijay Badge <147237638+TanviBadge@users.noreply.github.com>
2026-04-17 10:38:03 -0400

    Day 9 Q25: String length without strlen - iterate until null character

---
+++ 25th.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+
+int main() {
+    char str[100];
+    int length = 0, i;
+    
+    printf("Enter a string: ");
+    gets(str);
+    
+    for(i = 0; str[i] != '\0'; i++) {
+        length++;
+    }
+    
+    printf("Length of string = %d\n", length);
+    return 0;
+}
